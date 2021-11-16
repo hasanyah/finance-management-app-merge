97b3cbf49a3571dcf025e6d0ae73a7816d60e93f
Hasan Yahya <hasanyah@gmail.com>
2021-11-16 01:09:08 +0100

    Fetch splitwise data or use local test file

---
+++ fetchSplitwise.py
@@ -0,0 +1,29 @@
+import sys
+import configparser
+import requests
+import json
+
+jsondata = []
+if (len(sys.argv) == 1):
+    parser = configparser.RawConfigParser()
+    parser.read("config")
+    endpoint = parser.get("config", "endpoint")
+    cookie = parser.get("config", "cookie")
+    requestHeaders = {'Cookie' : cookie}
+
+    response = requests.get(endpoint, headers=requestHeaders)
+    if (response.status_code != 200):
+        print("There is a problem with the response!")
+        print(response.json())
+        quit()
+    else:
+        print("Server response has been successfully received!")
+        f = open("output.json", "a")
+        jsondata = f.write(json.dumps(response.json()))
+        f.close()
+else:
+    f = open(sys.argv[1], "r")
+    jsondata = f.read()
+    f.close()
+
+print(jsondata)
