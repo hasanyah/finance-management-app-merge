9b3a51e5ed0cb535214e710c17796c910a2a17a0
Hasan Yahya <hasanyah@gmail.com>
2021-11-16 02:12:08 +0100

    Extract id, cost, date, description, category from expenses

--- fetchSplitwise.py
+++ fetchSplitwise.py
@@ -2,11 +2,12 @@ import sys
 import configparser
 import requests
 import json
+from dataclasses import dataclass
 
 jsondata = []
+parser = configparser.RawConfigParser()
+parser.read("config.ini")
 if (len(sys.argv) == 1):
-    parser = configparser.RawConfigParser()
-    parser.read("config.ini")
     endpoint = parser.get("config", "endpoint")
     cookie = parser.get("config", "cookie")
     requestHeaders = {'Cookie' : cookie}
@@ -19,11 +20,38 @@ if (len(sys.argv) == 1):
     else:
         print("Server response has been successfully received!")
         f = open("output.json", "a")
-        jsondata = f.write(json.dumps(response.json()))
+        jsondata = json.dumps(response.json())
+        f.write(jsondata)
         f.close()
 else:
     f = open(sys.argv[1], "r")
     jsondata = f.read()
     f.close()
 
-print(jsondata)
+splitwiseUserId = int(parser.get("config", "splitwise_userid"))
+
+@dataclass
+class Expense:
+    id          : int
+    cost        : float
+    description : str
+    date        : str
+    category    : str
+
+personalExpenses = []
+expenses = json.loads(jsondata)["expenses"]
+for expense in expenses:
+    users = expense["users"]
+    for user in users:
+        if user["user_id"] == splitwiseUserId and float(user["owed_share"]) != 0.0:
+            personalExpenses.append(
+                Expense(
+                    id          = expense["id"],
+                    cost        = float(user["owed_share"]),
+                    description = expense["description"],
+                    date        = expense["date"],
+                    category    = expense["category"]["name"]
+                ))
+
+totalPersonalExpense = sum(expense.cost for expense in personalExpenses)
+print(totalPersonalExpense)
